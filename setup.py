92fdbd57c3fc13e1117e846ac1396ee3c67a2d93
mdipierro <massimo.dipierro@gmail.com>
2016-02-10 15:50:24 -0600

    fixed README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='taskutils',
-      version='0.1',
+      version='0.2',
       description='A multithreaded task scheduler for task queues and recurrent tasks',
       author='Massimo DiPierro',
       author_email='massimo.dipierro@gmail.com',
@@ -12,5 +12,6 @@ setup(name='taskutils',
       install_requires=[],
       py_modules=["taskutils"],
       license= 'BSD',
+      package_data = {'': ['README.md']},
       keywords='task scheduler',
       )
